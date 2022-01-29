2aee2dc04f471df91e351f2e620b5ec365b52423
akhpkn <akh.andr@mail.ru>
2022-01-29 16:58:35 +0300

    basic spring boot project

---
+++ src/main/java/com/example/learningplatform/LearningPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learningplatform/LearningPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
